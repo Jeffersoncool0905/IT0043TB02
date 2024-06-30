ecd4919d83e878c93e2eb1f7f2c5235d0daa1b66
Jefferson Paul M. Petronio <151502808+Jeffersoncool0905@users.noreply.github.com>
2024-06-30 22:29:42 +0800

    activity
    activity bukas

---
+++ Activity Monday/advance.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const flexboxContainer = document.querySelector('.flexbox-container');
+    const boxes = document.querySelectorAll('.box');
+
+    document.getElementById('resetFlexbox').addEventListener('click', () => {
+        flexboxContainer.style.gap = '0px';
+        flexboxContainer.style.flexDirection = 'row';
+        flexboxContainer.style.justifyContent = 'flex-start';
+        flexboxContainer.style.alignItems = 'flex-start';
+    });
+
+    document.getElementById('gap').addEventListener('input', (e) => {
+        flexboxContainer.style.gap = `${e.target.value}px`;
+    });
+
+    document.querySelectorAll('.flexDirection').forEach(button => {
+        button.addEventListener('click', (e) => {
+            flexboxContainer.style.flexDirection = e.target.dataset.direction;
+        });
+    });
+
+    document.querySelectorAll('.justifyContent').forEach(button => {
+        button.addEventListener('click', (e) => {
+            flexboxContainer.style.justifyContent = e.target.dataset.justify;
+        });
+    });
+
+    document.querySelectorAll('.alignItems').forEach(button => {
+        button.addEventListener('click', (e) => {
+            flexboxContainer.style.alignItems = e.target.dataset.align;
+        });
+    });
+
+    document.getElementById('resetGrow').addEventListener('click', () => {
+        boxes.forEach(box => {
+            box.style.flexGrow = '0';
+        });
+    });
+
+    document.getElementById('growAll').addEventListener('click', () => {
+        boxes.forEach(box => {
+            box.style.flexGrow = '1';
+        });
+    });
+
+    document.querySelectorAll('.flexGrowButton').forEach(button => {
+        button.addEventListener('click', (e) => {
+            const box = document.getElementById(`box${e.target.dataset.box}`);
+            const growValue = document.querySelector(`.flexGrow[data-box="${e.target.dataset.box}"]`).value;
+            box.style.flexGrow = growValue;
+        });
+    });
+});
---
+++ notesjava.js
@@ -0,0 +1,159 @@
+
+
+// let num1 = 0;
+// let surName;
+// const PI= 3.1416;
+
+
+
+
+// console.log(PI);
+// console.log(typeof(PI));
+
+// console.log(num1);
+// console.log(typeof(num1));
+
+// surname = "Tuazon";
+// surname = true;
+// console.log(surname);
+// console.log(typeof(surname));
+
+
+// console.log(surname);
+// console.log(typeof(surname));
+
+// console.log(surname);
+// console.log(typeof(surname));
+
+
+// variables
+// let email = "jeffersonpaulmpetronio@yahoo.com.ph";
+// let vocalist = "Mr. Jefferson";
+// let firstName = "Jefferson";
+// let lastName = "Petronio";
+// let num1 = 10;
+// let num2 = 10;
+// let allCaps = firstName.toUpperCase();
+// alert(email);
+
+// types
+// console.log(allCaps)
+// console.log(email);
+// console.log(typeof(email));
+// console.log(vocalist);
+// console.log(typeof(vocalist));
+// let sliced = email.slice(0,5);
+
+// stringslice
+// console.log(sliced)
+
+// pangilan
+// console.log(email.indexOf('@'));
+
+// console.log(firstName + ' ' +lastName);
+// console.log("The vocalist of Ben&Ben is " + firstName + " "+ lastName);
+
+// concatenation
+// console.log (`the vocalist of Ben&Ben is ${firstName} ${lastName}.`);
+// console.log(firstName + num1);
+// console.log(num1 + 2);
+// console.log(num1 + num2);
+// console.log(typeof(num1, num2));
+// console.log(firstName.length);
+
+
+// numbers
+// variables
+// + - */ ** %
+// let radius = 10;
+// const pi = 3.14;
+// let answer = radius ** 2 * pi;
+
+// console.log(answer);
+// console.log(`The are of a circle with a radius of
+// ${radius} is ${answer} centimeters.`);
+
+// radius = radius + 1;
+// radius++;
+// radius+=10;
+
+// console.log(radius);
+
+// let main_div = document.getElementById("main-div");
+// let song = "Pang-Kopiko";
+// let artist = "Beanie";
+// let plays = 100000000;
+
+// let complete = `
+// <h2>${song}</h2>
+// <p>${artist}</p>
+// <p>${plays}</p>
+// `;
+//  console.log(`The song ${song} by ${artist} already has ${plays} plays in spotify`);
+// main_div.innerHTML = complete;
+
+// console.log(complete); 
+
+//  variables
+// let chr = [];
+// chr[0] = 'mario';
+// chr[1] = 'luigi';
+// chr[2] = 'yoishi';
+// chr[3] = 100;
+// chr[4] = true;
+
+//  push and pop
+// chr.push('koopa');
+// chr.push('toad');
+// chr.pop();
+// chr.pop();
+
+//  console
+// console.log(chr[0]);
+// console.log(chr[1]);
+// console.log(chr[2]);
+// console.log(chr[3]);
+// console.log(chr[4]);
+// console.log(chr[5]);
+// console.log(chr[6]);
+//  logs of console
+// console.log(chr);
+
+
+
+// let num;
+
+// num1 ='kamote' + 10;
+// num2 ='kamote' + 10;
+
+
+// console.log(num1);
+// console.log(typeof(num));
+
+// console.log(num2);
+// console.log(typeof(num23));
+
+
+
+// bool;eans
+// true or false
+
+// variables
+// let num1 = 1;
+// let num2 = '1';
+
+// console output
+// console.log(num1 !==  num2);
+
+
+// let rad;
+// console.log(rad);
+// function compute(){
+//     rad= document.getElementById ("radius").value;
+//     let area = rad * rad * 3.14
+//     document.getElementById("area").textContent = `The are of a circle with radius of ${rad} is ${area} centimeters.`;
+//     rad.style.backgroundColor = "red";
+// }
+// console.log(area);
+
+ 
